863fa511630b67dd30a1ec0002619159627639ab
Howard Fernando Del Angel Leija <ferdelangel92@gmail.com>
2022-10-14 00:49:54 -0400

    git java

---
+++ src/main/java/com/mycompany/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+/**Document: index Created on: Jul. 23, 2022, 1:19:37 a.m.
+   Author: Ing. Howard Fenando Del Angel Leija**/
+
+package com.mycompany.helloworld;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
