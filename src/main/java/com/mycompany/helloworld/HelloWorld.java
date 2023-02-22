34aee2b1273f83cd9bc24ecc25dbeea1a2f77d3e
Howard Fernando Del Angel Leija <ferdelangel9221@aol.com>
2023-02-22 05:58:57 -0800

    modificated on HP Pavilion 21/02/2023

--- src/main/java/com/mycompany/helloworld/HelloWorld.java
+++ src/main/java/com/mycompany/helloworld/HelloWorld.java
@@ -11,8 +11,6 @@ public class HelloWorld
 
     public static void main(String[] args) 
     {
-        int myAge = 30;
-        int herAge = myAge;
-        System.out.println(herAge);
+                  
     }
 }
