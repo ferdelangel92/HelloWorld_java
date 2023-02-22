639fb209c127b7aaf86aaa2879046bb290f550dc
Howard Fernando Del Angel Leija <ferdelangel9221@aol.com>
2023-02-22 13:34:14 -0800

    modificated on HP Pavilion 22/02/2023

--- src/main/java/com/mycompany/helloworld/HelloWorld.java
+++ src/main/java/com/mycompany/helloworld/HelloWorld.java
@@ -11,6 +11,7 @@ public class HelloWorld
 
     public static void main(String[] args) 
     {
-                  
+        String message = "Hello World" + " !!!";        
+        System.out.println(message.replace("sky"));
     }
 }
