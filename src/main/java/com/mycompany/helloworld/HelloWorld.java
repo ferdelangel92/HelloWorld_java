f7d2c815c4009fb4e67364c395058412b742fbe7
Howard Fernando Del Angel Leija <ferdelangel9221@aol.com>
2023-02-21 19:53:19 -0800

    modificated on HP Pavilion 21/02/2023

--- src/main/java/com/mycompany/helloworld/HelloWorld.java
+++ src/main/java/com/mycompany/helloworld/HelloWorld.java
@@ -1,7 +1,7 @@
 /**
    Document: Hello World.
    Created on: Jul. 23, 2022, 1:19:37 a.m.
-   Author: Ing. Howard Fenando Del Angel Leija.
+   Author: Ing. Howard Fernando Del Angel Leija.
 **/
 
 package com.mycompany.helloworld;
@@ -11,6 +11,8 @@ public class HelloWorld
 
     public static void main(String[] args) 
     {
-        System.out.println("Hello World with Java!");
+        int myAge = 30;
+        int herAge = myAge;
+        System.out.println(herAge);
     }
 }
