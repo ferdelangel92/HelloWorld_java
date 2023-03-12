d37c222081c4df69f6f503a2198a7c3768a440be
fernando <fernando@fernando-HP-Compaq-Elite-8300-SFF>
2023-03-12 01:05:26 -0500

    calculator

--- src/main/java/com/mycompany/helloworld/HelloWorld.java
+++ src/main/java/com/mycompany/helloworld/HelloWorld.java
@@ -6,10 +6,44 @@
 
 package com.mycompany.helloworld;
 
+import java.util.Scanner;
+
 public class HelloWorld 
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello World with Java");
-    }
+	    final byte MONTHS_IN_YEAR = 12;
+	    final byte PERCENT = 100;
+	    
+	    int principal = 0;
+	    
+	    Scanner scanner = new Scanner(System.in);
+	    
+	    while(true)
+	    {
+		System.err.println("Principal: ");
+		int principal = scanner.nextInt();
+		if(principal >= 1000 && principal <=1_000_000)
+			break;
+		    System.out.println("Enter a value between 1000 and 1000000");
+	    }
+	    
+	    while (true)
+	    {
+		System.out.println("Annual Interest Rate: ");
+		float annualInterest = scanner.nextFloat();
+		if (annualInterest >=1 && annualInterest <= 30)
+		{
+			break;
+		}
+		float monthlyInterest = annualInterest / PERCENT / MONTHNS_IN_YEAR;
+	    }
+		    
+	    System.out.println("Period (Years): ");
+	    byte years = scanner.nextByte();
+	    int numberOfPayments = years * MONTHS_IN_YEAR;
+	    
+	    double mortgage = principal
+		    * ()
+	}
 }
